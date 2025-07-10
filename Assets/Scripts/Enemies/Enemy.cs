415ba5548fdef16ae5357c6ed415a34bffe931ae
Timofeev Nick <alvonsa09121985@gmail.com>
2025-07-10 20:16:17 +0400

    Update Enemy script, update Scene

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -2,13 +2,32 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    void Start()
+    [SerializeField] private float _speed;
+    [SerializeField] private float _distanceToPoint;
+    [SerializeField] private Transform[] _points;
+
+    private float DistanceToTarget
     {
-        
+        get => Vector3.Distance(transform.position, _points[_currentIndex].position);
     }
 
-    void Update()
+    private int _currentIndex = 0;
+
+    private void Update()
     {
-        
+        Vector3 targetPosition = new Vector3(_points[_currentIndex].position.x, transform.position.y, _points[_currentIndex].position.z);
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * _speed);
+
+        if (DistanceToTarget <= _distanceToPoint)
+        {
+            if (_currentIndex == _points.Length - 1)
+            {
+                Destroy(this.gameObject);
+
+                return;
+            }
+
+            _currentIndex++;
+        }
     }
 }
